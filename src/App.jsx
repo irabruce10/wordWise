a880a4e0199fc59c5d8bee49a207d947841fbc79
Bruce <irabruce10@gmail.com>
2024-03-20 09:11:03 +0100

    routing and spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-  function App() {
-  return <div>WordWise</div>;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<Product />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
-
