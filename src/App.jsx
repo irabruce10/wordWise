6586a6216e84ec109ff0096b3183a31dc4d089b6
Bruce <irabruce10@gmail.com>
2024-03-21 09:45:21 +0100

    routing and spa

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>list of cities</p>} />
             <Route path="cities" element={<p>list of cities</p>} />
             <Route path="countries" element={<p>list of countries</p>} />
             <Route path="form" element={<p>Form</p>} />
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,20 @@
-
+import { NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
 
 function AppNav() {
   return (
-    <div>AppNav</div>
-  )
+    <div className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default AppNav
+export default AppNav;
