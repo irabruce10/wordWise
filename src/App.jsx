8bbc3572caa56ac278a8aea0d1c0c6ce0332b84a
Bruce <irabruce10@gmail.com>
2024-03-21 09:33:11 +0100

    routing and spa

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route path="cities" element={<p>list of cities</p>} />
+            <Route path="countries" element={<p>list of countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
