d1addbbdd1d3f3046d6ea46f276105c304fcfb9d
Bruce <irabruce10@gmail.com>
2024-03-25 10:39:31 +0100

    implementing the Cities List

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
 import styles from "./CityList.module.css";
+import { useEffect } from "react";
 
+const Base_Url = "http://localhost:9000";
 function CityList() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await `${Base_Url}/cities`;
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an Arror ...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return <ul className={styles.cityList}>CityList</ul>;
 }
 
